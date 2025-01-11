8e98afa4633078c4db0ce7e4393a99e0da7d89bb
MirAkhlak <mirakhlak00@gmail.com>
2025-01-11 08:01:58 +0600

    Add devtools_options.yaml and settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+rootProject.name = "routine_tracker"
+
+// Include other projects if you have a multi-project setup
+// include(":app", ":module1", ":module2")
+
+// Additional configuration for plugin management
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+// Dependency resolution management
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
